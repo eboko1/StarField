3a7b7f70e91cc522893bf2f3456e4f90e68174ce
Victoria Frantsuh <eboko1@gmail.com>
2017-01-18 22:34:58 +0200

    Initial commit

---
+++ src/Star.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+/**
+ * Created by Vika on 17.01.2017.
+ */
+public class Star extends JFrame {
+    int x;
+    int y;
+    int z;
+
+
+        public final int WIDTH=400;
+        public final int HEIGHT=400;
+
+        public Star (){
+            setSize(WIDTH,HEIGHT);
+            setResizable(false);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            setVisible(true);
+            //add(new RPanel());
+        }
+        public static void main (String[] args) {
+            new Star();
+        }
+
+    }
+
+
