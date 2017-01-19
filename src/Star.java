4eb2a59c648b1fa618d61d5b9a43d270e6a0d66b
Victoria Frantsuh <eboko1@gmail.com>
2017-01-19 20:43:20 +0200

    adding class Panel

--- src/Star.java
+++ src/Star.java
@@ -9,21 +9,21 @@ public class Star extends JFrame {
     int z;
 
 
-        public final int WIDTH=400;
-        public final int HEIGHT=400;
+    public final int WIDTH=400;
+    public final int HEIGHT=400;
 
-        public Star (){
-            setSize(WIDTH,HEIGHT);
-            setResizable(false);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public Star (){
+        setSize(WIDTH,HEIGHT);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            setVisible(true);
-            //add(new RPanel());
-        }
-        public static void main (String[] args) {
-            new Star();
-        }
-
+        setVisible(true);
+        //add(new RPanel());
     }
+    public static void main (String[] args) {
+        new Star();
+    }
+
+}
 
 
---
+++ src/StarPanel.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Vika on 19.01.2017.
+ */
+public class StarPanel {
+
+    public void update(){
+
+    }
+}
