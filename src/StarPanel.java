b449aeed02d52addc2d7e2611d4328fcbc4df607
Victoria Frantsuh <eboko1@gmail.com>
2017-01-22 01:08:57 +0200

    adding oval

--- src/StarPanel.java
+++ src/StarPanel.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
  * Created by Vika on 19.01.2017.
  */
 public class StarPanel extends JPanel implements ActionListener{
+    StarField stars = new StarField();
+
     public StarPanel(){
 
             Timer timer = new Timer(40,this);
@@ -16,15 +18,26 @@ public class StarPanel extends JPanel implements ActionListener{
 
     public void update(){
 
+
+   // for (int i=0;i<stars.length;i++){
+   //     stars[i] = new StarField();
+   // }
+
     }
     public void paint(Graphics g){
         g.setColor(Color.red);
-        g.drawOval(3,5,30,40);
+        g.drawOval(3,5,9,9);
+        //for (int i=0;i<stars.length;i++){
+        //    stars[i] = update();
+
+      //  }
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        update();
+        repaint();
     }
 }
 
